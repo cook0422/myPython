ddaceb090c17e2d9052642b0c6b49db57d7e82c3
大小魔王 <yipeng0422@qq.com>
2019-07-08 19:22:28 +0800

    update

--- CookieGetByChrome.py
+++ CookieGetByChrome.py
@@ -1,9 +1,12 @@
 import os
 import sqlite3
-import requests
+import requests,urllib
 import win32crypt
+import json
 import xlrd
 import xlwt
+import re
+
 
 #from win32crypt import CryptUnprotectData
 def getcookiefromchrome(host='.taobao.com'):
@@ -12,7 +15,7 @@ def getcookiefromchrome(host='.taobao.com'):
     with sqlite3.connect(cookiepath) as conn:
         cu=conn.cursor()        
         cookies={name:win32crypt.CryptUnprotectData(encrypted_value)[1].decode() for host_key,name,encrypted_value in cu.execute(sql).fetchall()}
-        print(cookies)
+        #print(cookies)
         return cookies
 
 #运行环境windows 2012 server python3.4 x64 chrome 50
@@ -20,14 +23,31 @@ def getcookiefromchrome(host='.taobao.com'):
 
 
 url='https://sycm.taobao.com/adm/v2/downloadById.do?id=853524&reportType=1'
-httphead={'User-Agent':'Safari/537.36',}
+httphead={'User-Agent':'Safari/537.36','accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',}
+html = requests.get('https://sycm.taobao.com/adm/v2/execute/previewById.json?id=591115&reportType=1',headers=httphead,cookies=getcookiefromchrome('.taobao.com')).content
+html = json.loads(html)
+for money in html['data']['data']:
+        print(money[6])
+raise
 
 #设置allow_redirects为真，访问http://my.taobao.com/ 可以跟随跳转到个人空间
-r=requests.get(url,headers=httphead,cookies=getcookiefromchrome('.taobao.com'),allow_redirects=1).content
-with open("E:/Cook/git_pro/myPython/biaobiao.xls","wb") as f:
-    f.write(r)
+r=requests.get(url,headers=httphead,cookies=getcookiefromchrome('.taobao.com'),allow_redirects=1)
+r.encoding = "utf-8"
+requst_headers = json.loads(json.dumps(dict(r.headers)))
+file_name = re.search('filename=(.*)\"',requst_headers['Content-disposition'])
+if file_name:
+        file_name = file_name.group(1).replace("\\","").replace("\"","")      
+        print(file_name)  
+        file_name = urllib.parse.unquote(file_name,encoding='gb2312')
+else:
+        import time
+        file_name = print (time.strftime("%Y-%m-%d %H%M%S", time.localtime()))
+with open("E:/Cook/git_pro/myPython/" + file_name,"wb") as f:
+    f.write(r.content)
 
-data = xlrd.open_workbook('E:/Cook/git_pro/myPython/biaobiao.xls')
+
+raise
+data = xlrd.open_workbook('E:/Cook/git_pro/myPython/biaobiaos.xls')
 table = data.sheets()[0]
 row = table.nrows
 col = table.ncols
---
+++ autoReport.py
@@ -0,0 +1,76 @@
+import os
+import sqlite3
+import requests,urllib
+import win32crypt
+import json
+import re
+
+#from win32crypt import CryptUnprotectData
+def getcookiefromchrome(host='.taobao.com'):
+    cookiepath=os.environ['LOCALAPPDATA']+r"\Google\Chrome\User Data\Default\Cookies"
+    sql="select host_key,name,encrypted_value from cookies where host_key='%s'" % host
+    with sqlite3.connect(cookiepath) as conn:
+        cu=conn.cursor()        
+        cookies={name:win32crypt.CryptUnprotectData(encrypted_value)[1].decode() for host_key,name,encrypted_value in cu.execute(sql).fetchall()}
+        #print(cookies)
+        return cookies
+
+#运行环境windows 2012 server python3.4 x64 chrome 50
+#以下是测试代码
+
+class autoLoadReports(object):
+
+    httphead = {'User-Agent':'Safari/537.36','accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',}
+    cookies = getcookiefromchrome('.taobao.com')
+    ReportsListUlr = 'https://sycm.taobao.com/adm/v2/userReport/reportList.json?dateType=day'           #报表LIST地址
+    ReportsViewUrl = 'https://sycm.taobao.com/adm/v2/execute/previewById.json?id=%s&reportType=1'       #报表预览地址
+    ReportsDloadtUlr = 'https://sycm.taobao.com/adm/v2/downloadById.do?id=%s&reportType=1'              #下载报表地址
+    Repots = []
+
+
+    def __init__(self,file = "d:/"):       #初始化报表任务List
+        self.filepath = file
+        html = requests.get(self.ReportsListUlr,headers = self.httphead, cookies = self.cookies).content
+        list_json = json.loads(html)
+        for rp in list_json['data']:
+            temp_rp:report = report(str(rp['id']),rp['name'])
+            self.Repots.append(temp_rp)
+
+    #下载预览报表
+    def loadViewReports(self):
+        for rp in self.Repots:
+            print(rp.reportID  + ':' + rp.reportName)
+            html = requests.get(self.ReportsViewUrl % rp.reportID,headers=self.httphead,cookies=self.cookies).content
+            views_json = json.loads(html)
+            print(type(views_json['data']['size']))
+            for view in views_json['data']['data']:
+                print(view[1])
+
+    #下载报表
+    def loadReports(self):
+        for rp in self.Repots:
+            req=requests.get(self.ReportsDloadtUlr % rp.reportID,headers=self.httphead,cookies= self.cookies)
+            requst_headers = json.loads(json.dumps(dict(req.headers)))
+            file_name = re.search('filename=(.*)\"',requst_headers['Content-disposition'])
+            if file_name:
+                    file_name = file_name.group(1).replace("\\","").replace("\"","")      
+                    print(file_name)  
+                    file_name = urllib.parse.unquote(file_name,encoding='gb2312')
+            else:
+                    import time
+                    file_name = print (time.strftime("%Y-%m-%d %H%M%S", time.localtime()))
+            with open("E:/Cook/git_pro/myPython/" + file_name,"wb") as f:
+                f.write(req.content)
+
+
+"""
+报表类
+"""
+class report(object):
+    def __init__(self,id:str,name:str):
+        self.reportID = id
+        self.reportName = name
+
+
+test = autoLoadReports('E:/Cook/git_pro/myPython/')
+test.loadReports()
