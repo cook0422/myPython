84f45d63e335b357388ff16bf93295ebb63c6171
cook0422 <yipeng0422@qq.com>
2019-07-24 00:55:30 +0800

    update

--- taobaoApriori.py
+++ taobaoApriori.py
@@ -16,14 +16,16 @@ def main():
     items_data.drop(['标题','价格','外部系统编号','商品属性','套餐信息','备注','订单状态'],axis=1,inplace= True)  # 加1为列
     items_data["商家编码"] = items_data["商家编码"].str[0:9]
     items_data =items_data.dropna(axis = 0)
-    bool = items_data["商家编码"].str.contains('8182')
+    bool = items_data["商家编码"].str.contains('8181')
     items_data = items_data[bool]
     items_data = pd.pivot_table(data = items_data,values = '购买数量', index =['订单编号'] ,columns=['商家编码'],aggfunc=sum)
     items_data = items_data.fillna(0)
     items_data = items_data.applymap(encode_units)  #只能是0和1
 
-    frequent_itemsets = apriori(items_data, min_support=0.02, use_colnames=True)
+    frequent_itemsets = apriori(items_data, min_support=0.005, use_colnames=True)
     rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+    #antecedants前项集，consequents后项集，support支持度，confidence置信度，lift提升度。
+    rules = rules[ (rules['lift'] >= 6) &(rules['confidence'] >= 0.35) ].sort_values("lift",ascending = False)
     print(rules.head())
 
 if __name__ == "__main__":
